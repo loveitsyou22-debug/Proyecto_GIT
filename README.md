87300c18a08bf016d300be2db3b2350399f65c33
Jeremmy <gigachat224@gmail.com>
2026-01-09 17:07:08 -0500

    Primer commit: README y main.cpp

---
+++ README.md
@@ -0,0 +1,8 @@
+\# Proyecto de Control de Versiones
+
+
+
+Proyecto en C++ para aprender Git y GitHub.
+
+
+
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola Git" << endl;
+    return 0;
+}
